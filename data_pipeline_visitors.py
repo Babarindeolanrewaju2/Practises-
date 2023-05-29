a3f02ec833987335cfc3d0450ee91b9c81fb1386
Babarindeolanrewaju2 <Babarindeolanrewaju@gmail.com>
2023-05-29 19:21:40 +0100

    data pipeline in Python that calculates the number of visitors who have visited a website each day based on web server log data

---
+++ data_pipeline_visitors.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+# Step 1: Extraction
+def extract_logs(log_file):
+    # Code to extract web server log data from log files
+    logs = pd.read_csv(log_file, delimiter=" ")
+    return logs
+
+# Step 2: Transformation
+def transform_logs(logs):
+    # Clean and preprocess the log data
+    cleaned_logs = logs.dropna()  # Remove rows with missing values
+    cleaned_logs["timestamp"] = pd.to_datetime(cleaned_logs["timestamp"])  # Convert timestamp to datetime format
+
+    # Perform aggregation to calculate daily visitor count
+    daily_visitors = cleaned_logs.groupby(cleaned_logs["timestamp"].dt.date)["ip_address"].nunique()
+
+    return daily_visitors
+
+# Step 3: Loading
+def load_data(data):
+    # Code to load the transformed data into a data store or reporting tool
+    data.to_csv("/path/to/daily_visitors.csv", header=["date", "visitor_count"], index=True)
+    print("Daily visitor count saved to CSV file.")
+
+# Main function to execute the data pipeline
+def main():
+    try:
+        # Step 1: Extraction
+        logs = extract_logs("/path/to/web_server_logs.log")
+
+        # Step 2: Transformation
+        daily_visitors = transform_logs(logs)
+
+        # Step 3: Loading
+        load_data(daily_visitors)
+
+    except Exception as e:
+        print("Error occurred during data pipeline execution:")
+        print(str(e))
+
+if __name__ == "__main__":
+    main()
